e3006c9048500183d8db49899e0894f69af9781a
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-13 22:39:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -31,3 +31,5 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+long_planeteer_calls("axe", "earth", "wind", "fire")
