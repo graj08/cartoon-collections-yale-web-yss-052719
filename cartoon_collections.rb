ef7767c58377047f06675825a3f62f765384fe56
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-13 22:23:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,14 +6,12 @@ def roll_call_dwarves (list)# code an argument here
 end
 
 def summon_captain_planet (planeteer_calls)# code an argument here
-  call_upcased = String.new # Your code here
-  planeteer_calls.collect do |x|
-    call_upcased << planeteer_calls[x].shift.upcase
-    call_upcased.push(x)
-    call_upcased.push("!")
-    planeteer_calls[x] = call_upcased
-  end
-  planeteer_calls
+planeteer_calls.map do |x| 
+x.capitalize
+end
+planeteer_calls.collect  do |y|
+  y.push("!")
+end
 end
 
 def long_planeteer_calls# code an argument here
